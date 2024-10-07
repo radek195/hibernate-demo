52d443bc2654f0f8664a70a03cfe255799b5d10d
Radek <radoslaw.parol95@gmail.com>
2024-10-07 17:12:38 +0200

    LAZY and EAGER fetch types

--- src/main/java/org/example/HibernateDemo.java
+++ src/main/java/org/example/HibernateDemo.java
@@ -12,18 +12,18 @@ import java.util.List;
 public class HibernateDemo {
     public static void main(String[] args) {
 
-        SchoolSubject geometry = new SchoolSubject();
-        geometry.setId(105);
-        geometry.setName("Geometry");
-
-        SchoolSubject math = new SchoolSubject();
-        math.setId(48);
-        math.setName("Math");
-
-        Student student = new Student();
-        student.setId(188);
-        student.setName("Tom");
-        student.setSchoolSubject(List.of(geometry, math));
+//        SchoolSubject geometry = new SchoolSubject();
+//        geometry.setId(105);
+//        geometry.setName("Geometry");
+//
+//        SchoolSubject math = new SchoolSubject();
+//        math.setId(48);
+//        math.setName("Math");
+//
+//        Student student = new Student();
+//        student.setId(188);
+//        student.setName("Tom");
+//        student.setSchoolSubject(List.of(geometry, math));
 
         Configuration configuration = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -35,14 +35,15 @@ public class HibernateDemo {
 
         Transaction transaction = session.beginTransaction();
 
-        session.persist(geometry);
-        session.persist(math);
-        session.persist(student);
+//        session.persist(geometry);
+//        session.persist(math);
+//        session.persist(student);
 
         Student retrievedStudent = session.get(Student.class, 188);
+//        List<SchoolSubject> schoolSubjects = retrievedStudent.getSchoolSubject();
+        //see difference.png
         transaction.commit();
 
-        System.out.println(retrievedStudent);
 
     }
 }
--- src/main/java/org/example/model/entities/Student.java
+++ src/main/java/org/example/model/entities/Student.java
@@ -1,6 +1,7 @@
 package org.example.model.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
@@ -14,7 +15,7 @@ public class Student {
     @Id
     private int id;
     private String name;
-    @OneToMany(mappedBy = "student")
+    @OneToMany(mappedBy = "student", fetch = FetchType.LAZY) //FetchType.EAGER
     //student comes from SchoolSubject class
     private List<SchoolSubject> schoolSubject;
 
